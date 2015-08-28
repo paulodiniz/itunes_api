3274ed66d0a66850dc34d75c21ad48a86306649c
Paulo Diniz <paulo.rc.diniz@gmail.com>
2015-08-28 15:14:48 -0300

    Separating in headers method

--- app/models/itunes.rb
+++ app/models/itunes.rb
@@ -2,7 +2,7 @@ class Itunes
   include HTTParty
   attr_accessor :category_id, :monetization
 
-  base_uri 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewTop'
+  base_uri 'https://itunes.apple.com'
 
   def initialize(opts = {})
     @category_id  = opts[:category_id]
@@ -13,6 +13,23 @@ class Itunes
     @query ||= { genreId: @category_id, popId: 30, dataOnly: true, l: 'en'}
   end
 
+  def headers
+    @headers ||=
+      {
+        "Accept-Encoding"     => 'gzip, deflate, sdch',
+        "Accept-Language"     => 'en-US,en;q=0.8,lv;q=0.6',
+        "User-Agent"          => 'iTunes/11.1.1 (Windows; Microsoft Windows 7 x64 Ultimate Edition Service Pack 1 (Build 7601)) AppleWebKit/536.30.1',
+        "Accept"              => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        "Cache-Control"       => 'max-age=0',
+        "X-Apple-Store-Front" => '143441-1,17'
+      }
+  end
+
+  def options
+    @options ||= { query: query, headers: headers }
+  end
+
   def top_apps
+    self.class.get('/WebObjects/MZStore.woa/wa/viewTop', options)
   end
 end
--- spec/models/itunes_spec.rb
+++ spec/models/itunes_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Itunes', :type => :model do
 
-  it { expect(Itunes.base_uri).to eql 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewTop'}
+  it { expect(Itunes.base_uri).to eql 'https://itunes.apple.com'}
 
   describe '#initialize' do
     it 'must take category an monetization' do
@@ -16,6 +16,20 @@ RSpec.describe 'Itunes', :type => :model do
     it 'must return a hash with the correct params' do
       itunes = Itunes.new(category_id: 6003, monetization: :wtv)
       expect(itunes.query).to eql({ genreId: 6003, popId: 30, dataOnly: true, l: 'en' })
+    end
+  end
+
+  describe '#headers' do
+    it 'must return a hash with the correct headers' do
+      itunes = Itunes.new(category_id: 6003, monetization: :wtv)
+      headers = itunes.headers
+
+      expect(headers["Accept-Encoding"]).to eql('gzip, deflate, sdch')
+      expect(headers["Accept-Language"]).to eql('en-US,en;q=0.8,lv;q=0.6')
+      expect(headers["User-Agent"]).to eql('iTunes/11.1.1 (Windows; Microsoft Windows 7 x64 Ultimate Edition Service Pack 1 (Build 7601)) AppleWebKit/536.30.1')
+      expect(headers["Accept"]).to eql('text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+      expect(headers["Cache-Control"]).to eql('max-age=0')
+      expect(headers["X-Apple-Store-Front"]).to eql('143441-1,17')
     end
   end
 
