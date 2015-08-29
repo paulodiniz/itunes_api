7bf10015e98884217f880be5cfe84baaaebf98ae
Paulo Diniz <paulo.rc.diniz@gmail.com>
2015-08-29 15:50:52 -0300

    improving app_data model only to return necessary info

--- app/models/app_data.rb
+++ app/models/app_data.rb
@@ -9,42 +9,16 @@ class AppData
     self.metadata = fetch_metadata
   end
 
-  def track_id
-    metadata["trackId"]
-  end
-
-  def app_name
-    metadata["trackName"]
-  end
-
-  def description
-    metadata["description"]
-  end
-
-  def small_icon_url
-    metadata["artworkUrl60"]
-  end
-
-  def publisher_name
-    metadata["sellerName"]
-  end
-
-  def price
-    metadata["price"]
-  end
-
-  def version_number
-    metadata["version"]
-  end
-
-  def average_user_rating
-    metadata["averageUserRating"]
-  end
-
   private
   def fetch_metadata
     response = self.class.get('/lookup', {query: {id: app_id }})
     response_body = JSON.parse(response.body)
-    response_body["results"][0]
+    response_body["results"][0].slice("trackName",
+                "description",
+                "artworkUrl60",
+                "sellerName",
+                "price",  
+                "version",
+                "averageUserRating")
   end
 end
--- spec/models/app_data_spec.rb
+++ spec/models/app_data_spec.rb
@@ -8,14 +8,15 @@ RSpec.describe 'AppData', :type => :model do
     it 'must fetch metadata information' do
       VCR.use_cassette 'uber_metadata' do
         app_data = AppData.new(uber_id)
-        expect(app_data.track_id).to eql uber_id
-        expect(app_data.app_name).to eql "Uber"
-        expect(app_data.description).to include("Uber is your private driver in more than 50")
-        expect(app_data.small_icon_url).to eql "http://is4.mzstatic.com/image/thumb/Purple1/v4/e7/03/c0/e703c013-76b3-4fdb-eeee-838dc075fbd2/Icon.png/0x0ss-85.jpg"
-        expect(app_data.publisher_name).to eql "Uber Technologies, Inc."
-        expect(app_data.price).to eql 0.0
-        expect(app_data.version_number).to eql "2.92.1"
-        expect(app_data.average_user_rating).to eql 4.0
+        expect(app_data.metadata).to eql({
+          "trackName"         => "Uber",
+          "description"       => "Uber is your private driver in more than 50 countries.\n\n- Request a ride using the app and get picked up within minutes. On-demand service means no reservations required and no waiting in taxi lines.\n\n- Compare rates for different vehicles and get fare quotes in the app. Use PayPal or add a credit card to your secure account so you never need cash on hand.\n\n- Set your pickup location and final destination for an easy trip. Get connected to your personal driver and check the progress of your Uber at any time.\n\n- Sit back, relax, and go anywhere you want. We'll email you a receipt when you arrive at your destination.\n\nAn entirely new and modern way to travel is at your fingertips.",
+          "artworkUrl60"      => "http://is4.mzstatic.com/image/thumb/Purple1/v4/e7/03/c0/e703c013-76b3-4fdb-eeee-838dc075fbd2/Icon.png/0x0ss-85.jpg",
+          "sellerName"        => "Uber Technologies, Inc.",
+          "price"             => 0.0,
+          "version"           => "2.92.1",
+          "averageUserRating" => 4.0
+        })
       end
     end
   end
