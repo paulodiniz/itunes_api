263c973508a0d54ef6abc1e6d8080b3c3c9f989b
Paulo Diniz <paulo.rc.diniz@gmail.com>
2015-08-29 16:00:26 -0300

    def self instead of opening eigenclass

--- app/models/app_data.rb
+++ app/models/app_data.rb
@@ -2,19 +2,16 @@ class AppData
   include HTTParty
   base_uri 'https://itunes.apple.com'
 
-  class << self
-    def for(app_id)
-      response = get('/lookup', {query: {id: app_id }})
-      response_body = JSON.parse(response.body)
-      response_body["results"][0].slice("trackName",
-                  "description",
-                  "artworkUrl60",
-                  "sellerName",
-                  "price",
-                  "version",
-                  "averageUserRating")
-      
-    end
+  def self.for(app_id)
+    response = get('/lookup', {query: {id: app_id }})
+    response_body = JSON.parse(response.body)
+    response_body["results"][0].slice("trackName",
+                "description",
+                "artworkUrl60",
+                "sellerName",
+                "price",
+                "version",
+                "averageUserRating")
+    
   end
-
 end
